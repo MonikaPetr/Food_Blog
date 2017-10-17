b11994271f309694f576626c97263c98fbe9509a
MonikaPetr <petroniene.m@gmail.com>
2017-10-18 00:58:38 +0300

    v2

--- JS/food.js
+++ JS/food.js
@@ -1,15 +1,15 @@
 var input = document.getElementById("inputSpace");
 var inputButton = document.getElementById("navSearch");
-var icon = document.querySelector("i");
+var icon = document.getElementById("startSearch");
 
 function showInput() {
     input.classList.add("show")
-    inputButton.classList.add("show");
+    inputButton.classList.add("showButton");
     icon.classList.add("hide");
 }
 
 function hideInput() {
     input.classList.remove("show");
-    inputButton.classList.remove("show");
+    inputButton.classList.remove("showButton");
     icon.classList.remove("hide");
 }
